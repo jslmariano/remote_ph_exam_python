cd71c9ba6277f32bd41d467febd75d5de6601111
Josel Mariano <jslmariano@gmail.com>
2020-05-20 02:01:28 +0800

    Working copy of vueJS Layout * Moved angular to separate directory * Added new layout made from VUEJS * Deleted docker VUECLI for now * Pointed nginx config to parent workdir * Stayed vue docker compose file * Added customer/test API for dummy data

--- web/app/customers/controller/customer_controller.py
+++ web/app/customers/controller/customer_controller.py
@@ -1,3 +1,5 @@
+import copy
+
 from flask import request
 from flask_restplus import Resource
 
@@ -42,12 +44,51 @@ class CustomerCsv(Resource):
 
 
 @api.route('/test')
+@api.doc(params={'id': 'An ID'})
 class CustomerCsv(Resource):
 
     @api.doc('just_a_test')
     def get(self):
         """List all registered users"""
-        return {'message': 'Hello, I am your backend'}
+
+        test_datas = list()
+        row_amounts = list()
+
+        _row = dict()
+        _row['customer_company'] = 'MAMBA'
+        _row['customer_name'] = 'Edinburgh'
+        _row['order_date'] = '2011/04/25'
+        _row['delivered_amount'] = '61'
+        _row['total_amount'] = '320'
+
+        _limit = int(request.args.get('limit', 5))
+        _page = int(request.args.get('page', 1))
+        _term = request.args.get('term', None)
+        _start_date = request.args.get('start_date', None)
+        _end_date = request.args.get('end_date', None)
+
+
+        _offest = _limit * (_page - 1)
+        _max_to = _offest + _limit
+
+        for x in range(_offest, _max_to):
+            _item = copy.deepcopy(_row)
+            _item['order_name'] = '# CLKA -{}'.format(x)
+            _row_amount = int(_item['total_amount'])
+            test_datas.append(_item)
+            row_amounts.append(_row_amount)
+
+        results = dict()
+        results['total_amount'] = sum(row_amounts)
+        results['total_count'] = _max_to * 10
+        results['items'] = test_datas
+        results['page'] = _page
+        results['limit'] = _limit
+        results['_term'] = _term
+        results['_start_date'] = _start_date
+        results['_end_date'] = _end_date
+        results['message'] = 'Hello, I am your backend'
+        return results
 
 
 @api.route('/delete_all')
